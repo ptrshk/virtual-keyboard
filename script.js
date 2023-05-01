ba13b5d77bd76eb77d2c329dd4904151fa48d304
ptrshk <rostislavtrushko@gmail.com>
2023-05-01 14:39:48 +0300

    feat: add creating a keyboard functions

--- script.js
+++ script.js
@@ -70,3 +70,86 @@ let arrayLinesKeys = [firstLineKeys, secondLineKeys, thirdLineKeys, forthLineKey
 function addElement(element, place) {
   place.append(element);
 }
+
+addElement(textLanguage, document.body);
+addElement(textInput, document.body);
+addElement(wrapper, document.body);
+for (let i = 0; i < 5; i++) {
+  let line = document.createElement("div");
+  line.classList.add("line");
+  arrayLines.push(line);
+  addElement(arrayLines[i], wrapper);
+}
+
+let type = "ruSmall";
+function createRow() {
+  for (let j = 0; j < arrayLinesKeys.length; j++) {
+    let keys = arrayLinesKeys[j][type];
+    for (let i = 0; i < keys.length; i++) {
+      let button = document.createElement("button");
+      button.classList.add("button-small");
+      button.innerText = keys[i];
+      addElement(button, arrayLines[j]);
+    }
+  }
+}
+createRow();
+
+if (typePast) {
+  if (typePast[0] === "e") {
+    type = "enSmall";
+  } else if (typePast[0] === "r") {
+    type = "ruSmall";
+  }
+}
+
+function change() {
+  const save = JSON.stringify(type);
+  localStorage.setItem("myKey", save);
+  for (let j = 0; j < arrayLinesKeys.length; j++) {
+    if (arrayLinesKeys[j][type] !== undefined) {
+      let keys = arrayLinesKeys[j][type];
+      for (let i = 0; i < keys.length; i++) {
+        arrayLines[j].children[i].innerText = keys[i];
+      }
+    }
+  }
+}
+change();
+
+arrayLines[0].children[13].classList.add("flexBtn");
+arrayLines[1].children[0].classList.add("flexBtn");
+arrayLines[1].children[14].classList.add("flexBtn");
+arrayLines[2].children[0].classList.add("flexBtn");
+arrayLines[2].children[12].classList.add("flexBtn");
+arrayLines[2].children[12].id = "Enter";
+arrayLines[3].children[0].classList.add("flexBtn");
+arrayLines[3].children[11].classList.add("arrowUp");
+arrayLines[3].children[12].classList.add("flexBtn");
+
+for (let i = 0; i < 10; i++) {
+  if (i !== 3 && i < 9) {
+    arrayLines[4].children[i].classList.add("littleBtn");
+  }
+}
+arrayLines[4].children[3].classList.add("space");
+let deleteButton = arrayLines[1].children[14];
+let shiftLeftButton = arrayLines[3].children[0];
+let shiftRightButton = arrayLines[3].children[12];
+let controlLeftButton = arrayLines[4].children[0];
+arrayLines[4].children[0].classList.add('smallFont');
+let controlRightButton = arrayLines[4].children[8];
+arrayLines[4].children[8].classList.add("smallFont");
+let altLeftButton = arrayLines[4].children[2];
+arrayLines[4].children[2].classList.add("smallFont");
+let altRightButton = arrayLines[4].children[4];
+arrayLines[4].children[4].classList.add("smallFont");
+let winButton = arrayLines[4].children[1];
+arrayLines[4].children[1].classList.add("smallFont");
+let spaceButton = arrayLines[4].children[3];
+let arrowUpButton = arrayLines[3].children[11];
+let arrowLeftButton = arrayLines[4].children[5];
+let arrowDownButton = arrayLines[4].children[6];
+let arrowRightButton = arrayLines[4].children[7];
+let capsLockButton = arrayLines[2].children[0];
+
